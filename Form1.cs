f0dabca5b0013b0e8ae2043d50482da386f207f9
ByLeeKR <nutbooyoung@naver.com>
2020-04-14 17:07:56 +0900

    MySQL패키지 설치

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+//MySQL NuGet패키지 인스톨 및 라이브러리 선언
+//MySQL 설치 시 가급적 Uid / Password는 root로 부탁드립니다.
+//DB 포트는 3306으로 이용 부탁드립니다.
 
 namespace SOSIL_POS
 {
